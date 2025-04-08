02ab86c65e02eeea87d543c07f44d108eff56f81
MFkirin <hufercan@gmail.com>
2025-04-08 08:50:42 +0200

    coach rendering list

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,6 +5,24 @@
             <button>Refresh</button>
             <router-link to="/register">Register as Coach</router-link>
         </div>
-        <ul>List of Coaches</ul>
+        <ul v-if="hasCoaches">
+            <li v-for="coach in filterredCoaches" :key="coach">
+                {{ coach.firstName }}
+            </li>
+        </ul>
+        <h3 v-else> No coaches found</h3>
     </section>
 </template>
+
+<script>
+export default{
+    computed:{
+        filterredCoaches(){
+            return this.$store.getters['coaches/coaches']
+        },
+        hasCoaches(){
+            return this.$store.getters['coaches/hasCoaches']
+        }
+    }
+}
+</script>
