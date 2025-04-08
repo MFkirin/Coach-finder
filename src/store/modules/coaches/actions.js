2755aaddf403f5a47ef25383b3f35cee10f80302
MFkirin <hufercan@gmail.com>
2025-04-08 13:45:35 +0200

    Implement asynchronous coach registration and persist data using Firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,27 @@
 export default {
-    registerCoach(context, data){
-        const coachData ={
-            id:context.rootGetters.userId,
+    async registerCoach(context, data) {
+        const userId = context.rootGetters.userId;
+
+        const coachData = {
             firstName: data.first,
             lastName: data.last,
             description: data.desc,
             hourlyRate: data.rate,
             areas: data.areas
         };
-        context.commit('registerCoach', coachData);
+
+        const response = await fetch(`https://vue-http-demo-45729-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+            method: 'PUT',
+            body: JSON.stringify(coachData),
+        });
+
+        // const responseData = await responso.json();
+        if (!response.ok) {
+            //error
+        }
+        context.commit('registerCoach', {
+            ...coachData,
+            id: userId,
+        });
     }
 };
