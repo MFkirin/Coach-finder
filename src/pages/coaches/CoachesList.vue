b77d5583c080b2a41261997c65e4ad49415950f0
MFkirin <hufercan@gmail.com>
2025-04-08 12:16:31 +0200

    feat: form validation, register for coaches

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -4,7 +4,7 @@
         <base-card>
             <div class="controls">
                 <base-button mode="outline">Refresh</base-button>
-                <base-button link to="/register">Register as Coach</base-button>
+                <base-button v-if="!isCoach"  link to="/register">Register as Coach</base-button>
             </div>
             <ul v-if="hasCoaches">
                 <coach-item v-for="coach in filterredCoaches" :key="coach.id" :id="coach.id"
@@ -51,7 +51,10 @@ export default {
             });
         },
         hasCoaches() {
-            return this.$store.getters['coaches/hasCoaches']
+            return this.$store.getters['coaches/hasCoaches'];
+        },
+        isCoach(){
+            return this.$store.getters['coaches/isCoach'];
         }
     },
     methods: {
