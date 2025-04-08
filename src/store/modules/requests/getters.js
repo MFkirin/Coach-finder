28660c937fde8a4822283b38348068f0469529a0
MFkirin <hufercan@gmail.com>
2025-04-08 13:44:19 +0200

    Refactor getters for request filtering by user ID.

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,8 +1,9 @@
-export default{
-    requests(states){
-        return states.requests;
+export default {
+    requests(state, _, _2, rootGetters) {
+        const coachId = rootGetters.userId;
+        return state.requests.filter(req => req.coachId === coachId);
     },
-    hasRequests(state){
-        return state.requests && state.requests.length > 0;
+    hasRequests(_, getters) {
+        return getters.requests && getters.requests.length > 0;
     }
 }
